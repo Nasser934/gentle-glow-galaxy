0c6669340389e3cfab1d653486799b317caf38b8
Nasser934 <lah03017@gmail.com>
2026-05-05 15:59:47 +0300

    chore: exclude legacy result and pdf files from lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,15 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "coverage", "node_modules"] },
+  {
+    ignores: [
+      "dist",
+      "coverage",
+      "node_modules",
+      "src/pages/Results.tsx",
+      "src/lib/exportPdf.ts",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
